1141270b72674da0b97127fe6655ec59453bdd55
Omair Mohammed <126175145+omair-660@users.noreply.github.com>
2024-03-28 07:39:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+body = document.getElementById("body");
+moon = document.getElementById("moon");
+sun = document.getElementById("sun");
+
+moon.onclick = function () {
+  body.classList.add("dark");
+  moon.classList.add("none") ;
+  sun.classList.add("darkMode")  ;
+}
+sun.onclick = function () {
+    body.classList.remove("dark");
+    moon.classList.remove("none") ;
+  sun.classList.remove("darkMode")  ;
+}
+
+links = document.getElementById("links");
+hum = document.getElementById("hum");
+
+hum.onclick = function () {
+  links.classList.toggle("open");
+  hum.classList.toggle("active");
+}
