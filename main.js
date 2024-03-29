c2f2b0550969dddabe9c7aa3e9b98bae1b21c091
Omair Mohammed <126175145+omair-660@users.noreply.github.com>
2024-03-29 20:38:08 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -50,3 +50,24 @@ contact.onclick = function () {
   chefs.classList.remove("on");
   home.classList.remove("on");
 }
+document.oncontextmenu = function() {
+return false;
+};
+document.onkeydown = function(e) {
+if (e.keyCode == 123) {
+return false;
+}
+if (e.ctrlKey && e.shiftKey && e.keyCode ==  I .charCodeAt(0)) {
+return false;
+}
+if (e.ctrlKey && e.shiftKey && e.keyCode ==  J .charCodeAt(0)) {
+return false;
+}
+if (e.ctrlKey && e.keyCode ==  U .charCodeAt(0)) {
+return false;
+}
+if (e.ctrlKey && e.shiftKey && e.keyCode ==  C .charCodeAt(0)) {
+return false;
+}
+
+}
