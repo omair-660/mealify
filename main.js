15b56938ac79866c3710ad832bc41c282cdda9d6
Omair Mohammed <126175145+omair-660@users.noreply.github.com>
2024-03-29 03:57:33 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -20,3 +20,33 @@ hum.onclick = function () {
   links.classList.toggle("open");
   hum.classList.toggle("active");
 }
+
+home = document.getElementById("home");
+chefs = document.getElementById("chefs");
+gallery = document.getElementById("gallery");
+contact = document.getElementById("contact");
+
+home.onclick = function () {
+  home.classList.add("on");
+  gallery.classList.remove("on");
+  contact.classList.remove("on");
+  chefs.classList.remove("on");
+}
+chefs.onclick = function () {
+  chefs.classList.add("on");
+  home.classList.remove("on");
+  gallery.classList.remove("on");
+  contact.classList.remove("on");
+}
+gallery.onclick = function () {
+  gallery.classList.add("on");
+  chefs.classList.remove("on");
+  home.classList.remove("on");
+  contact.classList.remove("on");
+}
+contact.onclick = function () {
+  contact.classList.add("on");
+  gallery.classList.remove("on");
+  chefs.classList.remove("on");
+  home.classList.remove("on");
+}
